9e569646ef0ba6233be230696985de1fb5a7d475
Nikolas Lenning <nikolaslenning@gmail.com>
2020-12-02 12:30:49 -0700

    react horizonotal setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Alphabetical from "./pages/Alphabetical"
+import Home from "./pages/Home"
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/alphabetical">Alphabetical</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/job">Job</a>
+          </li>
+          
+        </ul>
+        <Switch>
+          <Route exact path="/" component ={Home} />
+          <Route exact path="/alphabetical" component ={Alphabetical} />
+
+        </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = (props) => {
+
+    return (
+        <div>
+            <h1>Make New Friends</h1>
+            <h2>Thumbs up on any pups you wanna meet!</h2>
+        </div>
+
+    );
+};
+
+export default Home;
