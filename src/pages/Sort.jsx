59bab5000c0848bad2d72cbf89e4c8c1b91e6b40
Nikolas Lenning <nikolaslenning@gmail.com>
2020-12-02 13:19:38 -0700

    implmented alphabetical sort

--- src/pages/Sort.jsx
+++ src/pages/Sort.jsx
@@ -8,12 +8,16 @@ class Sort extends React.Component {
     componentDidMount() {
       this.fetchUsers();
     }
-  
+    // sort alphabetically https://stackoverflow.com/questions/47998188/how-to-sort-an-object-alphabetically-within-an-array-in-react-js/47998260
     fetchUsers = async () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const data = await response.json();
-        console.log(data)
+        data.sort(function(a, b) {
+            if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+            if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+            return 0;
+           })
         this.setState({ users: data });
       } catch (error) {
         console.log(error);
@@ -26,7 +30,7 @@ class Sort extends React.Component {
           <h1>All Employees</h1>
   
           <ul>
-            {this.state.users.map(user => (
+            {this.state.users.sort().map(user => (
               <li key={user.id}>{user.name}</li>
             ))}
           </ul>
